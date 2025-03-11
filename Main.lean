ea15d9893d9fc42e08f872725a9821f095d16df9
Omar <146632459+omar826@users.noreply.github.com>
2025-03-12 00:11:23 +0530

    Representation
    rep.Lean represents any ncube

---
+++ Main.lean
@@ -0,0 +1,5 @@
+import ProofWidgets.Component.HtmlDisplay
+open scoped ProofWidgets.Jsx
+
+-- Click on the line below in the infoview to see the rendered HTML!
+#html <b>You can use HTML in Lean {.text s!"{1 + 3}"}!</b>
---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package n_n_n_Rubiks_Cube {
+  -- Add package configuration if needed
+}
+
+-- You should replace v0.0.3 with the latest version published under Releases
+
+
+
+lean_lib NNNRubiksCube
+
+lean_exe n_n_n_rubiks_cube {
+  root := `Main
+}
