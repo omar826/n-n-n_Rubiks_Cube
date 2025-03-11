5ccb3590efc5ac990fdfae2aaf4dafd47614e6c1
Omar <146632459+omar826@users.noreply.github.com>
2025-03-12 00:15:03 +0530

    proofwidgets

--- Main.lean
+++
@@ -1,5 +0,0 @@
-import ProofWidgets.Component.HtmlDisplay
-open scoped ProofWidgets.Jsx
-
--- Click on the line below in the infoview to see the rendered HTML!
-#html <b>You can use HTML in Lean {.text s!"{1 + 3}"}!</b>
